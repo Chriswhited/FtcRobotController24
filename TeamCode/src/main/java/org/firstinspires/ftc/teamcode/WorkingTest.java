4d30859d0c6f4a38732054559f0ddee0d313ea46
Chriswhited <87616817+Chriswhited@users.noreply.github.com>
2025-11-22 16:44:22 -0600

    created congif file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WorkingTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WorkingTest.java
@@ -18,160 +18,22 @@ import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.teamcode.mechanisms.config;
 
 @TeleOp(name = "WorkingTest", group = "Robot")
-@Disabled
+
 public class WorkingTest extends OpMode {
-    DcMotor back_left_drive;
-    DcMotor front_left_drive;
-    DcMotor back_right_drive;
-    DcMotor front_right_drive;
-    //DcMotor launch_motor_1;
-    DcMotor intake_motor;
-    //NormalizedColorSensor color1;
-    ColorSensor color1;
-
-    GoBildaPinpointDriver pinpoint;
-
-    IMU imu;
-
-    boolean intake_var;
-    float hsvValues[] = {0F,0F,0F};
-    ElapsedTime intake_timer = new ElapsedTime();
+    config conf = new config();
 
     @Override
-    public void init(){
-
-        back_left_drive = hardwareMap.get(DcMotor.class, "back_left_drive");
-        front_left_drive = hardwareMap.get(DcMotor.class, "front_left_drive");
-        back_right_drive = hardwareMap.get(DcMotor.class, "back_right_drive");
-        front_right_drive = hardwareMap.get(DcMotor.class, "front_right_drive");
-        //launch_motor_1 = hardwareMap.get(DcMotor.class, "launch_motor_1");
-        intake_motor = hardwareMap.get(DcMotor.class, "intake_motor");
-        //color1 = hardwareMap.get(ColorSensor.class, "color1");
-        pinpoint = hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
-        configurePinpoint();
-        pinpoint.setPosition(new Pose2D(DistanceUnit.INCH, 0, 0, AngleUnit.DEGREES, 0));
-
-        //launch_motor_1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        //launch_motor_1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        front_right_drive.setDirection(DcMotorSimple.Direction.REVERSE);
-        back_right_drive.setDirection(DcMotorSimple.Direction.REVERSE);
-        //launch_motor_1.setDirection(DcMotorSimple.Direction.REVERSE);
-        //pinpoint.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.REVERSED);
-
-
-        back_left_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        front_left_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        back_right_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        front_right_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        //launch_motor_1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        intake_motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        imu = hardwareMap.get(IMU.class, "imu");
-
-        RevHubOrientationOnRobot.LogoFacingDirection logoDirection =
-                RevHubOrientationOnRobot.LogoFacingDirection.UP;
-        RevHubOrientationOnRobot.UsbFacingDirection usbdirection =
-                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
-
-        RevHubOrientationOnRobot orientationOnRobot = new
-                RevHubOrientationOnRobot(logoDirection, usbdirection);
-        imu.initialize(new IMU.Parameters(orientationOnRobot));
+    public void init() {
+        conf.init(hardwareMap);
 
     }
 
     @Override
-    public void loop()  {
-        double front_left_power = -gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x;
-        double front_right_power = -gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x;
-        double back_right_power = -gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x;
-        double back_left_power = -gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x;
+    public void loop() {
+        telemetry.addLine("working");
 
-        double max_power = 1.0;
-
-        max_power = Math.max(max_power, Math.abs(front_left_power));
-        max_power = Math.max(max_power, Math.abs(front_right_power));
-        max_power = Math.max(max_power, Math.abs(back_right_power));
-        max_power = Math.max(max_power, Math.abs(back_left_power));
-
-        front_left_drive.setPower(front_left_power/max_power);
-        back_left_drive.setPower(back_left_power/max_power);
-        front_right_drive.setPower(front_right_power/max_power);
-        back_right_drive.setPower(back_right_power/max_power);
-
-        if(gamepad1.a && intake_var && intake_timer.seconds() > 0.5){
-            intake_motor.setPower(1);
-            intake_var = false;
-            intake_timer.reset();
-        }
-        else if(gamepad1.a && !intake_var && intake_timer.seconds() > 0.5){
-            intake_motor.setPower(0);
-            intake_var = true;
-            intake_timer.reset();
-        }
-
-        Color.RGBToHSV(color1.red() * 8, color1.green() * 8, color1.blue() * 8, hsvValues);
-        telemetry.addData("Red Value", (color1.red()));
-        telemetry.addData("Green Value", (color1.green()));
-        telemetry.addData("Blue Value", (color1.blue()));
-        telemetry.addData("Hue", hsvValues[0]);
-        telemetry.update();
-
-
-
-        telemetry.addLine("Push your robot around to see it track");
-        telemetry.addLine("Press B to reset the position");
-        if(gamepad1.b){
-            // You could use readings from April Tags here to give a new known position to the pinpoint
-            pinpoint.setPosition(new Pose2D(DistanceUnit.INCH, 0, 0, AngleUnit.DEGREES, 0));
-        }
-        pinpoint.update();
-        Pose2D pose2D = pinpoint.getPosition();
-
-        telemetry.addData("X coordinate (IN)", pose2D.getX(DistanceUnit.INCH));
-        telemetry.addData("Y coordinate (IN)", pose2D.getY(DistanceUnit.INCH));
-        telemetry.addData("Heading angle (DEGREES)", pose2D.getHeading(AngleUnit.DEGREES));
-    }
-    public void configurePinpoint(){
-        /*
-         *  Set the odometry pod positions relative to the point that you want the position to be measured from.
-         *
-         *  The X pod offset refers to how far sideways from the tracking point the X (forward) odometry pod is.
-         *  Left of the center is a positive number, right of center is a negative number.
-         *
-         *  The Y pod offset refers to how far forwards from the tracking point the Y (strafe) odometry pod is.
-         *  Forward of center is a positive number, backwards is a negative number.
-         */
-        pinpoint.setOffsets(-0.945, -6.5, DistanceUnit.INCH); //these are tuned for 3110-0002-0001 Product Insight #1
-
-        /*
-         * Set the kind of pods used by your robot. If you're using goBILDA odometry pods, select either
-         * the goBILDA_SWINGARM_POD, or the goBILDA_4_BAR_POD.
-         * If you're using another kind of odometry pod, uncomment setEncoderResolution and input the
-         * number of ticks per unit of your odometry pod.  For example:
-         *     pinpoint.setEncoderResolution(13.26291192, DistanceUnit.MM);
-         */
-        pinpoint.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
-
-        /*
-         * Set the direction that each of the two odometry pods count. The X (forward) pod should
-         * increase when you move the robot forward. And the Y (strafe) pod should increase when
-         * you move the robot to the left.
-         */
-        pinpoint.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD,
-                GoBildaPinpointDriver.EncoderDirection.FORWARD);
-
-        /*
-         * Before running the robot, recalibrate the IMU. This needs to happen when the robot is stationary
-         * The IMU will automatically calibrate when first powered on, but recalibrating before running
-         * the robot is a good idea to ensure that the calibration is "good".
-         * resetPosAndIMU will reset the position to 0,0,0 and also recalibrate the IMU.
-         * This is recommended before you run your autonomous, as a bad initial calibration can cause
-         * an incorrect starting value for x, y, and heading.
-         */
-        pinpoint.resetPosAndIMU();
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/config.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class config {
+
+    private DcMotor back_left_drive;
+    private DcMotor front_left_drive;
+    private  DcMotor back_right_drive;
+    private DcMotor front_right_drive;
+    private DcMotor launch_motor_1;
+    private DcMotor intake_motor;
+    // private ColorSensor color1;
+    private Servo franklin_flipper_right;
+    private Servo franklin_flipper_left;
+    private GoBildaPinpointDriver pinpoint;
+
+    public void init(HardwareMap hwMap)  {
+        back_left_drive = hwMap.get(DcMotor.class, "back_left_drive");
+        front_left_drive = hwMap.get(DcMotor.class, "front_left_drive");
+        back_right_drive = hwMap.get(DcMotor.class, "back_rigth_drive");
+        front_right_drive = hwMap.get(DcMotor.class, "front_right_drive");
+        launch_motor_1 = hwMap.get(DcMotor.class, "launch_motor_1");
+        intake_motor = hwMap.get(DcMotor.class, "intake_motor");
+//color1 = hwMap.get(ColorSensor.class, "color1");
+        pinpoint = hwMap.get(GoBildaPinpointDriver .class, "pinpoint");
+        franklin_flipper_right = hwMap.get(Servo .class, "franklin_flipper_right");
+        franklin_flipper_left = hwMap.get(Servo.class, "franklin_flipper_left");
+
+        launch_motor_1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        front_left_drive.setDirection(DcMotorSimple.Direction.REVERSE);
+        back_left_drive.setDirection(DcMotorSimple.Direction.REVERSE);
+        launch_motor_1.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        back_left_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        front_left_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        back_right_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        front_right_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        launch_motor_1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        intake_motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+}
+
