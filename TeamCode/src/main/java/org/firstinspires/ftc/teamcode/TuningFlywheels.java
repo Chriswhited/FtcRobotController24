ef0020176bf63bbafd2aaf85e72cc71d4b1ff9b4
K0lby <kolby35763@gmail.com>
2026-06-02 17:09:48 -0500

    Finished TuningFlywheels Program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TuningFlywheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TuningFlywheels.java
@@ -57,6 +57,11 @@ public class TuningFlywheels extends OpMode{
         telemetry.addData("P1", conf.launch_motor_1.getPower());
         telemetry.addData("P2", conf.launch_motor_2.getPower());
         telemetry.addData("Goal", speedVal);
+        //conf.pinpoint.update();
+        //Pose2D pose2D = conf.pinpoint.getPosition();
+        //telemetry.addData("X coordinate (IN)", pose2D.getX(DistanceUnit.INCH));
+        //telemetry.addData("Y coordinate (IN)", pose2D.getY(DistanceUnit.INCH));
+        //telemetry.addData("Heading angle (DEGREES)", pose2D.getHeading(AngleUnit.DEGREES));
 
 
         conf.dashboardTelemetry.addData("V1", conf.launch_motor_1.getVelocity());
@@ -68,7 +73,28 @@ public class TuningFlywheels extends OpMode{
         conf.dashboardTelemetry.update();
         telemetry.update();
 
-        if(gamepad2.right_trigger > 0.5) {
+        //Intake
+        Color.RGBToHSV(conf.colorRight.red() * 8, conf.colorRight.green() * 8, conf.colorRight.blue() * 8, conf.hsvValuesRight);
+        Color.RGBToHSV(conf.colorLeft.red() * 8, conf.colorLeft.green() * 8, conf.colorLeft.blue() * 8, conf.hsvValuesLeft);
+        Color.RGBToHSV(conf.colorCenter.red() * 8, conf.colorCenter.green() * 8, conf.colorCenter.blue() * 8, conf.hsvValuesCenter);
+        Color.RGBToHSV(conf.colorIntake.red() * 8, conf.colorIntake.green() * 8, conf.colorIntake.blue() * 8, conf.hsvValuesIntake);
+
+        if (conf.hsvValuesRight[0] > 140 && conf.hsvValuesLeft[0] > 140 && conf.hsvValuesCenter[0] > 145 && conf.hsvValuesIntake[0] > 145) {
+            if(conf.intake_timer.milliseconds() > 250) {
+                conf.intake_motor.setPower(-1);
+            }
+        }
+        else if (conf.hsvValuesRight[0] > 140 && conf.hsvValuesLeft[0] > 140 && conf.hsvValuesCenter[0] > 145 && conf.colorReadTimer.seconds() > 0.25) {
+            conf.intake_motor.setPower(0);
+            conf.intake_timer.reset();
+        }
+        else{
+            conf.intake_motor.setPower(1);
+            conf.intake_timer.reset();
+        }
+
+        //franklin flipper right
+        if(gamepad1.right_trigger > 0.5) {
             conf.franklin_flipper_right.setPosition(0.11);
         }
         else{
@@ -76,13 +102,14 @@ public class TuningFlywheels extends OpMode{
         }
 
         //franklin flipper left
-        if(gamepad2.left_trigger > 0.5) {
+        if(gamepad1.left_trigger > 0.5) {
             conf.franklin_flipper_left.setPosition(1);
         }
         else{
             conf.franklin_flipper_left.setPosition(0.64);
         }
 
+        //flywheel speed controls
         if(gamepad1.b){
             speedVal += 20;
         }
